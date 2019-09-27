73b43f1ca871c3d41fb88ef476425e7c58d29a82
Nnnastia <49305848+Nnnastia@users.noreply.github.com>
2019-09-27 16:21:40 +0300

    add SixtyNine project

---
+++ works/SixtyNine/wp-content/themes/sixtynine/footer.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package SixtyNine
+ */
+
+?>
+	<footer class="footer">
+		<div class="wrap">
+			<div class="footer-flex">
+				<div class="footer__item">
+					<a class="footer__link" href="#"><i class="fab fa-twitter footer__icon"></i></a>
+				</div>
+				<div class="footer__item">
+					<a class="footer__link" href="#"><i class="fab fa-facebook-f footer__icon"></i></a>
+				</div>
+				<div class="footer__item">
+					<a class="footer__link" href="#"><i class="fab fa-behance footer__icon"></i></a>
+				</div>
+				<div class="footer__item">
+					<a class="footer__link" href="#"><i class="fab fa-dribbble footer__icon"></i></a>
+				</div>
+				<div class="footer__item">
+					<a class="footer__link" href="#"><i class="fab fa-linkedin-in footer__icon"></i></a>
+				</div>
+			</div>
+			<div class="footer__text">All Copyrights reserved to Saied Hosni ©️ 2017</div>
+		</div>
+		<div class="up">
+			<a class="up__btm" href="#"><i class="fas fa-chevron-up"></i></a>
+		</div>
+	</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
